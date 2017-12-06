eedd462e4b743342b3137ea7b3d5133082e80fe2
Matthew Li <matthewli@D-10-18-227-148.dhcp4.washington.edu>
2017-12-06 11:36:31 -0800

    add tasklist and user dashboard

---
+++ mello/src/pages/UserDashboardPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import { Container, Col, Row } from 'reactstrap';
+import TaskList from '../components/TaskList';
+
+export default class LoginPage extends Component {
+
+    render() {
+
+        const styles = {
+            row : {
+                height: "50%"
+            },
+            container: {
+                height: "100%"
+            }
+        }
+
+        return (
+            <Container style={styles.container}>
+                <Row style={styles.row}>
+                    <Col xs="12">
+                        <TaskList />                                           
+                    </Col>
+                </Row>
+                <Row style={styles.row}>
+                    
+                </Row>
+            </Container>
+        );
+    }
+}
