7f4b29ae460656ab39bf2af309b7cb491f67c79f
ZKeiLin <zlin2016@uw.edu>
2017-12-07 04:19:35 -0800

    finish the basic form of the calendar

--- mello/src/components/test.js
+++ mello/src/components/test.js
@@ -9,15 +9,23 @@ export class Calendar extends Component{
         super(props);
         this.state ={
             events:this.props.events,
-            current: today.clone()
+            current: moment().clone()
         }        
+    }
+
+    rightHandler(){
+        // var update = this.state.title;
+        this.setState({current: this.state.current.add(1, "months")})
+    }
+    leftHandler(){
+        var update = today;
+        this.setState({current: this.state.current.subtract(1, "months")})
     }
 
     render(){
         return(
             <div id="calendar">
-            <Headers current={this.state.current}/>
-            {/* <Content /> */}
+            <Headers current={this.state.current} rightHandler={()=>this.rightHandler()} leftHandler={()=>this.leftHandler()}/>
             <Month  current={this.state.current}/>
             </div>
         );
@@ -31,21 +39,14 @@ class Headers extends Component{
             current: this.props.current
         }
     }
-    rightHandler(){
-        // var update = this.state.title;
-        this.setState({current: this.state.current.add(1, "months")})
-    }
-    leftHandler(){
-        var update = today;
-        this.setState({current: this.state.current.subtract(1, "months")})
-    }
+    
 
     render(){
         return(
             <div className="header">
                 <h1>{this.state.current.format('MMMM YYYY')}</h1>
-                <div className="right" onClick={()=>this.rightHandler()}></div>
-                <div className="left" onClick={()=>this.leftHandler()}></div>
+                <div className="right" onClick={this.props.rightHandler}></div>
+                <div className="left" onClick={this.props.leftHandler}></div>
             </div>
         );
     }
@@ -57,25 +58,28 @@ class Month extends Component{
     }
 
     current(){
-        var clone = this.props.current.clone();
+        var clone = this.props.current.date(1).clone();
         var weekInMonth = [];
         var key = 0;
-        while(clone.month() === this.props.current.month()) {           // content.push(<Day key={key} day={clone.format} />);
+        while(clone.month() === this.props.current.date(1).month()) {           // content.push(<Day key={key} day={clone.format} />);
             // content.push(clone.add('days', 1).format());
-        weekInMonth.push(<Day key={clone.format()} day={moment(clone)} current={this.props.current} />)
-        clone.add('days', 1);    
+            weekInMonth.push(<Day key={clone.format()} day={moment(clone)} current={this.props.current} />)
+            clone.add('days', 1);
+            // console.log(clone);
         }
         return weekInMonth;
     }
 
     backfill(){
         var backfill = []
-        var clone = this.props.current.clone();
+        var clone = this.props.current.date(1).clone();
+        console.log(clone);
         var dayOfWeek = clone.day();
         if(!dayOfWeek) { return; }
-        clone.subtract('days', dayOfWeek+1);
+        clone.subtract(dayOfWeek+1,'days').format;
+        console.log(dayOfWeek);
         for(var i = dayOfWeek; i > 0 ; i--) {
-            var theDay = clone.add(1, 'days').format();      
+            var theDay = clone.add(1, 'days').format(); 
             backfill.push(<Day key={theDay} day={moment(theDay)} current={this.props.current} />)            
         } 
         return backfill;      
@@ -83,7 +87,7 @@ class Month extends Component{
 
     forwardfill(){
         var forwardfill = []
-        var clone = this.props.current.clone().add('months', 1).subtract('days', 1);
+        var clone = this.props.current.date(1).clone().add('months', 1).subtract('days', 1);
         var dayOfWeek = clone.day();
         if(dayOfWeek === 6) { return; }
         for(var i = dayOfWeek; i < 6 ; i++) {
@@ -92,8 +96,8 @@ class Month extends Component{
         }
         return forwardfill;
     }
-    
-    render(){
+
+    combine(){
         let weekInMonth = this.current();
         let backfill = this.backfill();
         let forwardfill = this.forwardfill();
@@ -104,15 +108,11 @@ class Month extends Component{
         }else{
             all = backfill.concat(weekInMonth, forwardfill);
         }
-        let content = [];
-        var week = []
-        all.map((date)=>{
-            week.push(date);
-            if(moment(date.key).day() === 0){
-                content.push(<Week key={date} days={week}></Week>)
-                week = [];
-            }
-        })
+        return all;
+    }
+
+    render(){
+        let content = this.combine();
         
         return(
             <div>
@@ -125,22 +125,19 @@ class Month extends Component{
 
 
 
-class Week extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        // var content =[];
-        // this.props.days.map((day)=>{
-        //     content.push(<Day key={day} day={moment(day)} current={this.props.current} />)
-        // })
-        return(
-            <div className="week">
-                {this.props.days}
-            </div>
-        );
-    }
-}
+// class Week extends Component{
+//     constructor(props){
+//         super(props);
+//     }
+//     render(){
+
+//         return(
+//             <div className="week">
+//                 {this.props.days}
+//             </div>
+//         );
+//     }
+// }
 
 class Day extends Component{
     constructor(props){
@@ -150,12 +147,15 @@ class Day extends Component{
             open : false
         }
     }
+    handleClick(){
+        this.setState({open : !this.state.open})
+    }
 
     getClassName(day){
         var classes = ['day'];        
         if(day.month() !== this.props.current.month()) {
           classes.push('other');
-        } else if (this.props.current.isSame(day, 'day')) {
+        } else if (moment().isSame(day, 'day')) {
           classes.push('today');
         }
         return classes.join(' ');
@@ -165,22 +165,22 @@ class Day extends Component{
         var className = this.getClassName(this.props.day);
         var theDay = this.props.day;   
         var content;
-        if(this.props.day.day() === 0) {  
-        }   ;
         return(
             
             // outer
-            <div className={""+className}>
-            {/* {if(this.state.week || day.day() === 0)} */}
+            <div className={""+className} onClick={()=>this.handleClick()}>
                 <div className="day-name">{theDay.format('ddd')}</div>
-                <div className="day-number">{theDay.format('DD')}</div>
-                {/* <div className="day-events"> */}
+                <div className="day-number">{theDay.format('DD')}
+                
+                {this.state.open  &&
+                <div className="day-events">
                 {/* <Event day={this.props.day}/> */}
-                {/* <div className="details in">
+                    <div className="details in">
                     <div className="arrow"></div>
-                </div> */}
-
-               
+                    </div>
+                </div>
+                }
+                </div>
             </div>
         );
     }
