cfcbb632d43c3d42b6ba97b57fc1765079adb38a
ZKeiLin <zlin2016@uw.edu>
2017-12-07 23:07:57 -0800

    Update backup.js

--- mello/src/components/backup.js
+++ mello/src/components/backup.js
@@ -1,43 +1,37 @@
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
-import React, { Component } from 'react';
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 import events from 'events';
 import firebase from 'firebase/app';
 import 'firebase/database';
 
+import React, { Component } from 'react';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
 BigCalendar.momentLocalizer(moment);
-let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
-
-
 
 // generating all the event(tasks)
 export default class Calendar extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            events : []
-        }
     }
-    componentDidMount() {   
-        this.groupRef = firebase.database().ref("Task");
-        this.groupRef.on('value', (snapshot) => {
-            console.log(snapshot.val());
-            this.setState({ events: snapshot.val() });
-        })
-        console.log(this.state.events);
-    }   
-
   render(){
-    var today = new Date();
     return (
-        // <div >
         <BigCalendar
-        style={{"width":"200px", "height":"200px"}}
         {...this.props}
-        events={this.state.events}
-        views={allViews}
-        step={60}
+        events={[
+            {
+              'title': 'All Day Event very long title',
+              'allDay': true,
+              'start': new Date(2015, 3, 0),
+              'end': new Date(2015, 3, 1)
+            },
+            {
+              'title': 'Long Event',
+              'start': new Date(2015, 3, 7),
+              'end': new Date(2015, 3, 10)
+            }]}
+        
+        views={["month", "week"]}
         defaultDate={new Date()}
       />
     )
